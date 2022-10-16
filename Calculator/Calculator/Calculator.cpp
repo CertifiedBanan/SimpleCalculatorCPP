b7a9b05e9f0baf521667a013a52712f5cecac822
CertifiedBanan <93042053+CertifiedBanan@users.noreply.github.com>
2022-10-16 14:51:37 +0200

    Add files via upload

---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double firstNumber;
+    string operatorNumbers;
+    double secondNumer;
+
+    cout << "Welcome to my Calculator!\n"
+            "This is only for improving my C++ skills.\n"
+            "Enter a number\n";
+    cin >> firstNumber;
+    cout << "Do you want to Multiply, Divide, Plus or Minus?\n";
+    cout << "Example: plus\n";
+    cin >> operatorNumbers;
+    cout << "Enter a second number\n";
+    cin >> secondNumer;
+    cout << "Calculating...\n";
+
+    if (operatorNumbers == "multiply")
+    {
+        double multipliedNumber = firstNumber * secondNumer;
+        cout << multipliedNumber;
+    }
+
+    if (operatorNumbers == "divide")
+    {
+        double dividedNumber = firstNumber / secondNumer;
+        cout << dividedNumber;
+    }
+
+    if (operatorNumbers == "plus")
+    {
+        double plusNumber = firstNumber + secondNumer;
+        cout << plusNumber;
+    }
+
+    if (operatorNumbers == "minus")
+    {
+        double minusNumber = firstNumber - secondNumer;
+        cout << minusNumber;
+    }
+
+    return EXIT_SUCCESS;
+}
